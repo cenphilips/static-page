c95351271848600298acaf8d48acdf87c81f5d13
charlykso <charlykso141@gmail.com>
2025-07-05 18:06:03 +0100

    created a js folder and index.js file

---
+++ js/index.js
@@ -0,0 +1,28 @@
+// this is the begining of our class
+/**
+ * This
+ * is
+ * multi line comment
+ */
+let x = 10;
+const pi = 3.142;
+let y = 5;
+let firstname = "Ikenna" 
+let lastname = "Remigius"
+
+console.log(firstname +" "+ lastname)
+console.log(typeof(x))
+console.log(typeof(pi))
+console.log(typeof(2))
+/**
+ * String
+ * Number
+ * Bigint
+ * Boolean
+ * Undefined
+ * Null
+ * Symbol
+ * Object
+ */
+let gender = "";
+console.log(`My firstname is: ${firstname} and my last name is: ${lastname}`)
