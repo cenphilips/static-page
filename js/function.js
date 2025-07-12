ed5bb5f08e553f76aa5f26822388470bfa681014
charlykso <charlykso141@gmail.com>
2025-07-12 18:03:56 +0100

    introduction to js functions

---
+++ js/function.js
@@ -0,0 +1,15 @@
+/**
+ * function function_name()
+ * {
+ *  statement;
+ * }
+ */
+
+function add(){
+    let x = 10;
+    let y = 20;
+    let sum = x + y;
+    return sum;
+}
+let sum = add()
+console.log(sum)
