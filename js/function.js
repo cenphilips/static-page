2c74cdeffbfb77815b12e1dd646d4ce6018aef6b
cenphilips <cenphilips@gmail.com>
2025-07-18 05:31:11 +0100

    added some function

--- js/function.js
+++ js/function.js
@@ -5,17 +5,49 @@
  * }
  */
 
-function add(){
+/* function add(){
     let x = 10;
     let y = 20;
     return x + y;
-}
-let sum = add()
-console.log(sum)
+    }
+    let sum = add()
+    console.log(sum)
+*/
 
 function addNum(x, y){
     return x + y;
-}
-let sum1 = addNum(10, 20)
-console.log(sum1)
+    }
+    let add = addNum(20, 10);
+    console.log(add)
 
+
+function subNum(x, y){
+    return x - y;
+    }
+    let sub= subNum(40, 30);
+    console.log(sub)
+
+
+function mulNum(x, y){
+    return x*y;
+    }
+    let mul= mulNum(6, 4);
+    console.log(mul)
+
+function divNum(x, y){
+    return x/y;
+}
+    let div= divNum(19, 4);
+    console.log(div);
+
+function remainderNum(x, y){
+    return x%y;
+}
+    let remainder= remainderNum(19, 4);
+    console.log(remainder)
+
+function exponentNum(x, y){
+    return x**y;
+}
+    let exponent= exponentNum(3, 2)
+    console.log(exponent)
