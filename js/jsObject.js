d8c85e48bca9cc1a1f6343bdebdb98d61b1b7bb8
charlykso <charlykso141@gmail.com>
2025-09-15 17:23:51 +0100

    object methods

--- js/jsObject.js
+++ js/jsObject.js
@@ -1,5 +1,6 @@
 const Person = {
-    name: "Ikenna",
+    firstname: "Ikenna",
+    lastname: "Remigius",
     age: 25,
     gender: "Male",
     height: 1.8,
@@ -7,9 +8,16 @@ const Person = {
     complexion: "Fair",
     eyeColor: "Brown",
     hairColor: "Black",
+    fullname: "",
     // talk: function(){
     //     return "Hello"
     // }
+    get getGender(){
+        return this.gender
+    },
+    set setFullname(myname){
+        this.fullname = myname
+    }
 }
 Person.address = "123 Main Street"
 for(let key in Person){
@@ -22,3 +30,43 @@ console.log(jsonPerson)
 let { name, age, gender } = Person;
 console.log(name)
 console.log(age)
+
+
+const obj = { counter: 0};
+
+Object.defineProperty(obj, "reset", {
+    get: function() {
+        this.counter = 0
+    }
+})
+
+Object.defineProperty(obj, "increment", {
+    get: function(){
+        return this.counter++
+    }
+})
+
+Object.defineProperty(obj, "decrement", {
+    get: function(){
+        return this.counter--
+    }
+})
+
+Object.defineProperty(obj, "add", {
+    set: function(value){
+        this.counter += value
+    }
+})
+
+Object.defineProperty(obj, "subtract", {
+    set: function(i){
+        this.counter -= i
+    }
+})
+
+obj.add = 5
+console.log(obj.counter)
+obj.subtract = 2
+console.log(obj.counter)
+obj.reset;
+console.log(obj.counter)
