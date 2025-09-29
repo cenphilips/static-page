ad7b269341cc621275a78dbb31b36955f514f757
cenphilips <cenphilips@gmail.com>
2025-09-29 17:17:43 +0100

    added task_01.html, task_01.js and task_01.css

---
+++ js/task_01.js
@@ -0,0 +1,31 @@
+const header = document.getElementById('main-header')
+
+const nav = document.createElement('nav');
+nav.classList.add('navbar');
+
+const ul = document.createElement('ul');
+
+const navItems = [
+    { text: 'Home', href: 'task_01.html'},
+    { text: 'About', href: 'about.html'},
+    { text: 'Products', href: 'products.html'},
+    { text: 'Contact', href: 'contact.html'},
+];
+
+navItems.forEach( item => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+
+    a.textContent = item.text;
+    a.href = item.href;
+
+    li.appendChild(a);
+    ul.appendChild(li)
+});
+
+nav.appendChild(ul);
+header.appendChild(nav);
+
+let table = document.createElement('table')
+table.setAttribute("id", "studentTable")
+document.body.appendChild(table)
