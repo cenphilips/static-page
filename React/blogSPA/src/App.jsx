3a89dbfe4880030457ec17d2ae4f5b47f768bed2
cenphilips <cenphilips@gmail.com>
2025-10-27 17:08:43 +0100

    added product and service page

--- React/blogSPA/src/App.jsx
+++ React/blogSPA/src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 import About from './About'
+import Products from './Products'
+import Contact from './Contact'
+import Services from './Services'
+
 
 
 function App() {
@@ -12,9 +16,9 @@ function App() {
     <>
      <h1 className='text-3xl bg-black text-white text-center'>Welcome to React class</h1>
      <p>My name is {name}</p>
-     <button onClick={callSetName}>Change name</button>
-     <button onClick={() => setName("Ikenna")}>Reset name</button>
-      <About />
+     <button onClick={callSetName} className='bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-4 rounded mr-2'>Change name</button>
+     <button onClick={() => setName("Ikenna")} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded'>Reset name</button>
+      <Services />
     </>
   )
 }
