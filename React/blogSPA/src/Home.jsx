ab77630ae901cb4093b5b2551df685bc3f5f11c7
charlykso <charlykso141@gmail.com>
2025-11-12 17:49:23 +0100

    react hooks

--- React/blogSPA/src/Home.jsx
+++ React/blogSPA/src/Home.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
+  const [count, setCount] = useState(0)
   return (
     <div className=''>
-        <div className='flex aligns-center justify-center my-5'>
-            <h1 className='text-3xl'>Welcome to our Blog SPA Home Page</h1>
+      <div className='my-5'>
+        <div>
+          <h1 className='text-3xl block text-center'>Welcome to our Blog SPA Home Page</h1>
         </div>
+        <div className='flex flex-col items-center mt-5'>
+          <h2 className='text-2xl my-3 text-center'>Counter: {count}</h2>
+          <div>
+            <button
+              className='bg-blue-500 text-white px-4 py-2 mx-2 rounded cursor-pointer'
+              onClick={() => setCount(count + 1)}
+            >
+              Increment
+            </button>
+            <button
+              className='bg-red-500 text-white px-4 py-2 mx-2 rounded cursor-pointer'
+              onClick={() => count > 0 && setCount(count - 1)}
+            >
+              Decrement
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
