2e503d3736ae6d1d057859d84bd40c40def95893
charlykso <charlykso141@gmail.com>
2025-11-12 18:36:22 +0100

    react json data

--- React/blogSPA/src/Home.jsx
+++ React/blogSPA/src/Home.jsx
@@ -1,12 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Home = () => {
   const [count, setCount] = useState(0)
+  const [students, setStudents] = useState([])
+
+
+  useEffect(() => {
+    fetch("http://localhost:3000/Students")
+      .then((res) => res.json())
+      .then((data) => setStudents(data))
+      .catch((err) => console.log(err));
+  }, [])
   return (
     <div className=''>
       <div className='my-5'>
         <div>
-          <h1 className='text-3xl block text-center'>Welcome to our Blog SPA Home Page</h1>
+          <h1 className='text-3xl block text-center'>
+            Welcome to our Blog SPA Home Page
+          </h1>
         </div>
         <div className='flex flex-col items-center mt-5'>
           <h2 className='text-2xl my-3 text-center'>Counter: {count}</h2>
@@ -23,6 +34,30 @@ const Home = () => {
             >
               Decrement
             </button>
+          </div>
+          <div className='mt-10 w-full p-10'>
+            <table className='table-auto w-full border-collapse border border-gray-400'>
+              <thead className='px-4 py-2 bg-gray-200 border'>
+                <tr>
+                  <th className='px-4 py-2'>ID</th>
+                  <th className='px-4 py-2'>First Name</th>
+                  <th className='px-4 py-2'>Last Name</th>
+                  <th className='px-4 py-2'>Age</th>
+                  <th className='px-4 py-2'>Major</th>
+                </tr>
+              </thead>
+              <tbody>
+                {students.map((student) => (
+                  <tr key={student.id}>
+                    <td className='border px-4 py-2'>{student.id}</td>
+                    <td className='border px-4 py-2'>{student.firstname}</td>
+                    <td className='border px-4 py-2'>{student.lastname}</td>
+                    <td className='border px-4 py-2'>{student.age}</td>
+                    <td className='border px-4 py-2'>{student.major}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
