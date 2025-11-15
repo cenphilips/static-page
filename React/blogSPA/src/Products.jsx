9ea4ba65dff7ac8b2690dc19289ca8a1e320dae8
cenphilips <cenphilips@gmail.com>
2025-11-15 17:25:49 +0100

    modified products page

--- React/blogSPA/src/Products.jsx
+++ React/blogSPA/src/Products.jsx
@@ -16,8 +16,27 @@ useEffect(() => {
     <div>
       <h1>This is our Products page</h1>
       <div>{props.children}</div>
-      <div>
-        
+      <div className='mt-10 w-full p-10'>
+        <table className='table-auto w-full border-collapse border border-gray-400'>
+          <thead className='px-4 py-2 bg-gray-200 border'>
+            <tr>
+              <th className='px-4 py-2'>ID</th>
+              <th className='px-4 py-2'>Product Name</th>
+              <th className='px-4 py-2'>Prices</th>
+              <th className='px-4 py-2'>Vendor</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product.id}>
+                <td className='border px-4 py-2'>{product.id}</td>
+                <td className='border px-4 py-2'>{product.name}</td>
+                <td className='border px-4 py-2'>{product.price}</td>
+                <td className='border px-4 py-2'>{product.vendor}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
     
