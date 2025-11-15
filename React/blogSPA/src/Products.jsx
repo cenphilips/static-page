c12902b1343cef738fbf2909c9cf1c6eea3bcd6d
cenphilips <cenphilips@gmail.com>
2025-11-15 17:08:38 +0100

    added data to db.json

--- React/blogSPA/src/Products.jsx
+++ React/blogSPA/src/Products.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+
 
 const Products = (props) => {
+
+  const [products, setProducts] = useState([])
+
+useEffect(() => {
+        fetch("http://localhost:3000/Products")
+        .then((res) => res.json())
+        .then((data) => setProducts(data))
+        .catch((err) => console.log(err));
+}, [])
+
   return (
     <div>
       <h1>This is our Products page</h1>
       <div>{props.children}</div>
+      <div>
+        
+      </div>
     </div>
+    
   )
 }
 
